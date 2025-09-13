55ff2dc082c79a231884441a4b1ee9b5b8dc6478
rajucreate <svaradaraju8@gmail.com>
2025-09-14 04:05:06 +0530

    added backend

---
+++ src/BusDetails.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BusDetails extends Component {
+  render() {
+    return (
+      <>
+        <div>BusDetails</div>
+      </>
+    )
+  }
+}
