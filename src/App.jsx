df8a682ba0dcf5521646241e9bccefccea2774c8
Nayan-Balla <2400031211@kluniversity.in>
2025-09-14 00:09:15 +0530

    Created the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './App2.css'
-
+import React, { useRef, useState } from 'react';
+import './App.css';
+import './App2.css'; // If this holds extra styles
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Font Awesome
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
 
   return (
-    <>
-      <div id="main">
-        
-        <div class="bg"></div>
-        
+    <div
+      ref={containerRef}
+      className={`container ${isActive ? 'active' : ''}`}
+      id="container"
+    >
 
-        <div id="box">
-
-          <h1>Login</h1>
-
-          <div id="input">
-            <input type="text" placeholder='UserId'/>
-            <input type="password" placeholder='Password'/>
+      <div className="form-container sign-up">
+        <form>
+          <h1>Create Account</h1>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fab fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fab fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fab fa-github"></i></a>
+            <a href="#" className="icon"><i className="fab fa-linkedin-in"></i></a>
           </div>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>Sign Up</button>
+        </form>
+      </div>
 
-            <a href="#">Forgot Password</a><br />
+      
+      <div className="form-container sign-in">
+        <form>
+          <h1>Sign In</h1>
+          <div className="social-icons">
+            <a href="#" className="icon"><i className="fab fa-google-plus-g"></i></a>
+            <a href="#" className="icon"><i className="fab fa-facebook-f"></i></a>
+            <a href="#" className="icon"><i className="fab fa-github"></i></a>
+            <a href="#" className="icon"><i className="fab fa-linkedin-in"></i></a>
+          </div>
+          <span>or use your email and password</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
 
-            <button>Login</button>
-
-            <p class='ForSignup'>Account does not exist? <a href="#">Sign up</a></p>
-
+      
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Welcome Back!</h1>
+            <p>Enter your personal details to use all of our site’s features</p>
+            <button className="hidden" onClick={() => setIsActive(false)}>Sign In</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hello, Friend!</h1>
+            <p>Register with your personal details to use all of our site’s features</p>
+            <button className="hidden" onClick={() => setIsActive(true)}>Sign Up</button>
+          </div>
         </div>
-        
-        </div>
-    </>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
